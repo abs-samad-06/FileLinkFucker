8384e307dfd93918ee5bda2977d0815039635241
abs-samad-06 <mseditz.016@gmail.com>
2026-01-11 08:58:23 +0530

    Create __init__.py

---
+++ bot/__init__.py
@@ -0,0 +1,12 @@
+# bot/__init__.py
+
+"""
+FileLinkFucker Telegram Bot
+---------------------------
+Core bot package initializer.
+
+ABS Studio
+"""
+
+__version__ = "0.1.0"
+__author__ = "ABS Studio"
